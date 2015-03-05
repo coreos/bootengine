c2abd8fe86135d7118c4addf1ba66c7eecff5942
Bassam Tabbara <bassam@symform.com>
2015-03-05 14:39:36 -0800

    use a generator for root-mkfs

--- dracut/05root-mkfs/module-setup.sh
+++ dracut/05root-mkfs/module-setup.sh
@@ -3,10 +3,12 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 depends() {
-	return 0
+    echo systemd
 }
 
 install() {
     dracut_install mkfs.btrfs mkfs.ext4 mkfs.xfs
-    inst_simple "$moddir/diskless-btrfs" "$systemdutildir/root-mkfs"
+    inst_simple "$moddir/root-mkfs" "$systemdutildir/root-mkfs"
+    inst_simple "$moddir/root-mkfs-generator" \
+    	"$systemdutildir/system-generators/root-mkfs-generator"
 }
