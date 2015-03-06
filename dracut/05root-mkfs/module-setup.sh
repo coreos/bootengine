64ed73311238310fa4498fcc76e6f29a0ad9ddde
Bassam Tabbara <bassam@symform.com>
2015-03-05 22:46:49 -0800

    switch to using dracut pre-mount

--- dracut/05root-mkfs/module-setup.sh
+++ dracut/05root-mkfs/module-setup.sh
@@ -3,12 +3,10 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 depends() {
-    echo systemd
+    return 0
 }
 
 install() {
     dracut_install mkfs.btrfs mkfs.ext4 mkfs.xfs
-    inst_simple "$moddir/root-mkfs" "$systemdutildir/root-mkfs"
-    inst_simple "$moddir/root-mkfs-generator" \
-    	"$systemdutildir/system-generators/root-mkfs-generator"
+    inst_hook pre-mount 05 "$moddir/pre-mount-root-mkfs.sh"
 }
--- dracut/05root-mkfs/pre-mount-root-mkfs.sh
+++ dracut/05root-mkfs/pre-mount-root-mkfs.sh
@@ -1,19 +1,38 @@
-#!/bin/bash -x
+#!/bin/bash
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-root=$1
-rootmkfs=$2
-rootmkfstype=$3
-rootmkfsflags="$4"
+# Usage: cmdline_arg name default_value
+cmdline=( $(</proc/cmdline) )
+cmdline_arg() {
+    local name="$1" value="$2"
+    for arg in "${cmdline[@]}"; do
+        if [[ "${arg%%=*}" == "${name}" ]]; then
+            value="${arg#*=}"
+        fi
+    done
+    echo "${value}"
+}
 
-# check if the root exists
-if [[ `blkid ${root} > /dev/null` || `blkid -t ${root} > /dev/null` ]]; then
-    echo "root ${root} exists. rootmkfs will be ignored. continuing."
+root=$(cmdline_arg root)
+rootmkfs=$(cmdline_arg rootmkfs)
+rootmkfstype=$(cmdline_arg rootmkfstype btrfs)
+rootmkfsflags=$(cmdline_arg rootmkfsflags "-m single -l 32768 -n 32768 -L ROOT")
+
+if [[ ${rootmkfs} == "" ]]; then
     exit 0
 fi
 
-ls /dev/sd*
+if [[ ${root} == "" ]]; then
+    echo "the kernel option 'root' must be specified along with rootmkfs."
+    exit 1
+fi
+
+# check if the root exists
+if [[ `blkid -o device ${root}` || `blkid -o device -t ${root}` ]]; then
+    echo "root ${root} exists. rootmkfs will be ignored. continuing."
+    exit 0
+fi
 
 # check if the selected device is a block device
 if [[ ! -b ${rootmkfs} ]]; then
